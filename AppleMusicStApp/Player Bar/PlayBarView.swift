c9b281b943b06a52064286519ffb36589a0a5081
fachidiot <jerrykim428@gmail.com>
2021-12-16 16:41:48 +0900

    first commit

---
+++ AppleMusicStApp/Player Bar/PlayBarView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayBarView.swift
+//  AppleMusicStApp
+//
+//  Created by 김학동 on 2021/11/22.
+//  Copyright © 2021 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class PlayBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
